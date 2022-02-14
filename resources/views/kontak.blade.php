2a26fad3e52d2562b34e2e4afd623a19e6d31d3f
kayla0204 <kaylaayu0204@gmail.com>
2022-02-14 10:21:58 +0700

    firs commit

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('judul_halaman', 'Halaman Kontak')
+
+@section('konten')
+
+        <p> Ini Adalah Halaman Kontak</p>
+       <marquee> <p>Selamat datang ya guys ya !</p> </marquee>
+
+
+@endsection
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('judul_halaman', 'Halaman Tentang')
+
+@section('konten')
+
+        <p> Ini Adalah Halaman Tentang</p>
+        <p>Selamat datang !</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sint tempora voluptatum esse earum consequatur vel
+            vitae, odit dicta, eligendi deserunt molestiae rem atque magni amet repudiandae fugit saepe adipisci!</p>
+@endsection
